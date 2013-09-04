d31f0d0faafbc658c98f28b109c8af82c361b453
bspkrs <bspkrs@gmail.com>
2013-09-04 00:17:49 -0400

    couple of updates towards 1.6.2 compat

--- common/morethantrees/proxy/ClientProxy.java
+++ common/morethantrees/proxy/ClientProxy.java
@@ -1,6 +1,5 @@
 package morethantrees.proxy;
 
-
 import morethantrees.MTJT;
 import morethantrees.entity.EntityFireBalls;
 import morethantrees.entity.RenderFireBalls;
@@ -25,25 +24,25 @@ import morethantrees.items.EntityTnTStick;
 import morethantrees.items.RenderTnTStick;
 import morethantrees.util.MTJTKeyHandler;
 import net.minecraft.client.model.ModelBiped;
-import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderZombie;
 import cpw.mods.fml.client.registry.KeyBindingRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy
 {
-	@Override
-	public void registerRenderThings()
-	{
-		RenderingRegistry.registerEntityRenderingHandler(EntityTallChicken.class, new RenderLiving(new ModelBiped(), 0.5f));
-		RenderingRegistry.registerEntityRenderingHandler(EntityEasySpider.class, new RenderEasySpider());
-		RenderingRegistry.registerEntityRenderingHandler(EntitySkyChicken.class, new RenderSkyChicken(new ModelSkyChicken(), 0.5f));
-		RenderingRegistry.registerEntityRenderingHandler(EntityBlueCow.class, new RenderBlueCow(new ModelBlueCow(), 0.5f));
-		RenderingRegistry.registerEntityRenderingHandler(EntityZombieCow.class, new RenderZombie());
-		RenderingRegistry.registerEntityRenderingHandler(EntityGoat.class, new RenderGoat(new ModelGoat(), 0.5f));
-		RenderingRegistry.registerEntityRenderingHandler(EntityPunSheep.class, new RenderPunSheep(new ModelPunSheep2(), new ModelPunSheep1(), 0.7F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityTnTStick.class, new RenderTnTStick(0));
-		RenderingRegistry.registerEntityRenderingHandler(EntityFireBalls.class, new RenderFireBalls(0.5F));
-		KeyBindingRegistry.registerKeyBinding(new MTJTKeyHandler(MTJT.instance.KEY_UP, MTJT.instance.KEY_DOWN, MTJT.instance.KEY_IDLE, MTJT.instance.KEY_FORWARD, MTJT.instance.KEY_BACKWARD));
-	}
+    @Override
+    public void registerRenderThings()
+    {
+        RenderingRegistry.registerEntityRenderingHandler(EntityTallChicken.class, new RenderBiped(new ModelBiped(), 0.5f));
+        RenderingRegistry.registerEntityRenderingHandler(EntityEasySpider.class, new RenderEasySpider());
+        RenderingRegistry.registerEntityRenderingHandler(EntitySkyChicken.class, new RenderSkyChicken(new ModelSkyChicken(), 0.5f));
+        RenderingRegistry.registerEntityRenderingHandler(EntityBlueCow.class, new RenderBlueCow(new ModelBlueCow(), 0.5f));
+        RenderingRegistry.registerEntityRenderingHandler(EntityZombieCow.class, new RenderZombie());
+        RenderingRegistry.registerEntityRenderingHandler(EntityGoat.class, new RenderGoat(new ModelGoat(), 0.5f));
+        RenderingRegistry.registerEntityRenderingHandler(EntityPunSheep.class, new RenderPunSheep(new ModelPunSheep2(), new ModelPunSheep1(), 0.7F));
+        RenderingRegistry.registerEntityRenderingHandler(EntityTnTStick.class, new RenderTnTStick(0));
+        RenderingRegistry.registerEntityRenderingHandler(EntityFireBalls.class, new RenderFireBalls(0.5F));
+        KeyBindingRegistry.registerKeyBinding(new MTJTKeyHandler(MTJT.instance.KEY_UP, MTJT.instance.KEY_DOWN, MTJT.instance.KEY_IDLE, MTJT.instance.KEY_FORWARD, MTJT.instance.KEY_BACKWARD));
+    }
 }
